a1a9f549b8a0676bf0f3f7b6f8291829f21a795c
NgocTu <giabang205@gmail.com>
2024-01-24 01:38:20 +0700

    Initial react application and the template demo

---
+++ CSharp/CSharp12News/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+
+public class Animal(string name, int age)
+{
+    public string Name => name;
+    public int Age => age;
+
+    public Animal Parent { get; set; }
+
+    public void Eat()
+    {
+        Console.WriteLine("The animal is eating.");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine("The animal is sleeping.");
+    }
+
+    public void MakeSound()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var animal = new Animal("Rex", 3);
+        animal.MakeSound();
+
+        Func<int, int, bool> testForEquality = (x, y) => x == y;
+    }
+}
--- CSharp/TestVS/TestVS.V2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TestVS.V2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
