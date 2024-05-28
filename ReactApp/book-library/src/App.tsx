3b38b3063c12a08af6f596c6dcc3f13be6f6264a
Le Ngoc Tu <ngoctu@Les-MacBook-Pro.local>
2024-05-29 06:10:47 +0700

    remove react-app stack

--- ReactApp/book-library/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-import { RouterProvider } from "react-router-dom";
-import Router from "./routes/Router";
-
-const App: FC = () => <RouterProvider router={Router}></RouterProvider>;
-
-export default App;
